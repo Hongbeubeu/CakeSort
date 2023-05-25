d97ae11f6a3c536d06b5bfb557d5bde14895c3fa
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2023-05-25 20:36:23 +0700

    Fix

--- Assets/_CakeSort/Scripts/GamePlay/Core/BoardController.cs
+++ Assets/_CakeSort/Scripts/GamePlay/Core/BoardController.cs
@@ -3,114 +3,120 @@ using UnityEngine;
 
 public class BoardController : MonoBehaviour
 {
-	[SerializeField] private BoardSettings _settings;
-	[SerializeField] private Cell[] _cells;
-	private readonly Dictionary<Vector2, Cell> _managedCells = new();
+    [SerializeField] private BoardSettings _settings;
+    [SerializeField] private Cell[] _cells;
+    private readonly Dictionary<Vector2, Cell> _managedCells = new();
 
-	public void InitCells()
-	{
-		ResetBoard();
-		if (_managedCells.Count == 0)
-		{
-			var minX = -(_settings.BoardSize.x / 2f - _settings.CellSize / 2f);
-			var minY = -(_settings.BoardSize.y / 2f - _settings.CellSize / 2f);
-			var currentPos = new Vector2(minX, minY);
-			var index = 0;
-			for (var row = 0; row < _settings.BoardSize.y; row++)
-			{
-				currentPos.x = minX;
-				for (var col = 0; col < _settings.BoardSize.x; col++)
-				{
-					_managedCells.Add(currentPos, _cells[index]);
-					currentPos.x += _settings.CellSize;
-					index++;
-				}
+    public void InitCells()
+    {
+        ResetBoard();
+        if (_managedCells.Count == 0)
+        {
+            var minX = -(_settings.BoardSize.x / 2f - _settings.CellSize / 2f);
+            var minY = -(_settings.BoardSize.y / 2f - _settings.CellSize / 2f);
+            var currentPos = new Vector2(minX, minY);
+            var index = 0;
+            for (var row = 0; row < _settings.BoardSize.y; row++)
+            {
+                currentPos.x = minX;
+                for (var col = 0; col < _settings.BoardSize.x; col++)
+                {
+                    _managedCells.Add(currentPos, _cells[index]);
+                    currentPos.x += _settings.CellSize;
+                    index++;
+                }
 
-				currentPos.y += _settings.CellSize;
-			}
-		}
+                currentPos.y += _settings.CellSize;
+            }
+        }
 
-		foreach (var cell in _managedCells)
-		{
-			cell.Value.DestroyPlate();
-		}
-	}
+        foreach (var cell in _managedCells)
+        {
+            cell.Value.DestroyPlate();
+        }
+    }
 
-	public bool CanPlacePlate(Vector2 position, out Vector2 gridPosition)
-	{
-		gridPosition = ConvertToGrid(position);
-		if (!_managedCells.ContainsKey(gridPosition))
-			return false;
-		return !_managedCells[gridPosition].IsContainPlate;
-	}
+    public bool CanPlacePlate(Vector2 position, out Vector2 gridPosition)
+    {
+        gridPosition = ConvertToGrid(position);
+        if (!_managedCells.ContainsKey(gridPosition))
+            return false;
+        return !_managedCells[gridPosition].IsContainPlate;
+    }
 
-	public void AddPlate(Vector2 gridPosition, Plate plate)
-	{
-		if (!_managedCells.ContainsKey(gridPosition))
-		{
-			Debug.LogError($"Not exist grid {gridPosition}");
-			return;
-		}
+    public void AddPlate(Vector2 gridPosition, Plate plate)
+    {
+        if (!_managedCells.ContainsKey(gridPosition))
+        {
+            Debug.LogError($"Not exist grid {gridPosition}");
+            return;
+        }
 
-		_managedCells[gridPosition].SetPlate(plate);
-	}
+        _managedCells[gridPosition].SetPlate(plate);
+    }
 
-	public void RemovePlateAt(Vector2 gridPosition)
-	{
-		if (!_managedCells.ContainsKey(gridPosition))
-		{
-			Debug.LogError($"Not exist grid {gridPosition}");
-			return;
-		}
+    public void RemovePlateAt(Vector2 gridPosition)
+    {
+        if (!_managedCells.ContainsKey(gridPosition))
+        {
+            Debug.LogError($"Not exist grid {gridPosition}");
+            return;
+        }
 
-		_managedCells[gridPosition].DestroyPlate();
-	}
+        _managedCells[gridPosition].DestroyPlate();
+    }
 
-	private void ResetBoard()
-	{
-		foreach (var cell in _managedCells)
-		{
-			cell.Value.DestroyPlate();
-		}
-	}
+    private void ResetBoard()
+    {
+        foreach (var cell in _managedCells)
+        {
+            cell.Value.DestroyPlate();
+        }
+    }
 
-	public void FindNeighbourHasCakeId(Plate plate, int id)
-	{
-		Vector2 currentPos = plate.transform.position;
-		foreach (var t in _settings.Directions)
-		{
-			var pos = currentPos + t;
-			if (!_managedCells.ContainsKey(pos) || !_managedCells[pos].IsContainPlate)
-			{
-				continue;
-			}
+    public void FindNeighbourHasCakeId(Plate plate, int id)
+    {
+        Vector2 currentPos = plate.transform.position;
+        foreach (var t in _settings.Directions)
+        {
+            var pos = currentPos + t;
+            if (!_managedCells.ContainsKey(pos) || !_managedCells[pos].IsContainPlate)
+            {
+                continue;
+            }
 
-			if (_managedCells[pos].Plate.HasCake(id))
-			{
-				_managedCells[pos].Plate.MoveCakeToTarget(plate, id);
-			}
-		}
-	}
+            if (!_managedCells[pos].Plate.HasCake(id)) continue;
 
-	#region Utils
+            if (_managedCells[pos].Plate.NumberTypeOfCake >= plate.NumberTypeOfCake)
+            {
+                _managedCells[pos].Plate.MoveCakeToTarget(plate, id);
+            }
+            else
+            {
+                plate.MoveCakeToTarget(_managedCells[pos].Plate, id);
+            }
+        }
+    }
 
-	private Vector2 ConvertToGrid(Vector2 worldPosition)
-	{
-		var result = Vector2.zero;
-		var minX = -(_settings.BoardSize.x / 2f);
-		var minY = -(_settings.BoardSize.y / 2f);
+    #region Utils
 
-		var deltaX = worldPosition.x - minX;
-		var deltaY = worldPosition.y - minY;
+    private Vector2 ConvertToGrid(Vector2 worldPosition)
+    {
+        var result = Vector2.zero;
+        var minX = -(_settings.BoardSize.x / 2f);
+        var minY = -(_settings.BoardSize.y / 2f);
 
-		var delta = deltaX >= 0 ? _settings.CellSize / 2f : -_settings.CellSize / 2f;
-		result.x = minX + (int) (deltaX / _settings.CellSize) + delta;
+        var deltaX = worldPosition.x - minX;
+        var deltaY = worldPosition.y - minY;
 
-		delta = deltaY >= 0 ? _settings.CellSize / 2f : -_settings.CellSize / 2f;
-		result.y = minY + (int) (deltaY / _settings.CellSize) + delta;
+        var delta = deltaX >= 0 ? _settings.CellSize / 2f : -_settings.CellSize / 2f;
+        result.x = minX + (int) (deltaX / _settings.CellSize) + delta;
 
-		return result;
-	}
+        delta = deltaY >= 0 ? _settings.CellSize / 2f : -_settings.CellSize / 2f;
+        result.y = minY + (int) (deltaY / _settings.CellSize) + delta;
 
-	#endregion
+        return result;
+    }
+
+    #endregion
 }
--- Assets/_CakeSort/Scripts/GamePlay/Core/Plate.cs
+++ Assets/_CakeSort/Scripts/GamePlay/Core/Plate.cs
@@ -9,222 +9,247 @@ using Random = UnityEngine.Random;
 
 public class Plate : MonoBehaviour
 {
-	[SerializeField] private SpriteRenderer _spriteRenderer;
-	[SerializeField, ReadOnly] private List<Cake> _cakes = new();
-	[SerializeField] private int _rootSortingOrder;
-	private PlateSettings _settings;
-	private Vector3 spawnPosition;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField, ReadOnly] private List<Cake> _cakes = new();
+    [SerializeField] private int _rootSortingOrder;
+    private PlateSettings _settings;
+    private Vector3 spawnPosition;
 
-	public int LastIndex => (_cakes[^1].IndexInPlate + 1) % _settings.MaxPiecePerPlate;
-	public bool IsFullOfCake => _cakes.Count == _settings.MaxPiecePerPlate;
-	public bool IsCompleted => CheckComplete();
+    public int LastIndex => (_cakes[^1].IndexInPlate + 1) % _settings.MaxPiecePerPlate;
+    public bool IsFullOfCake => _cakes.Count == _settings.MaxPiecePerPlate;
+    public bool IsCompleted => CheckComplete();
+    public int NumberTypeOfCake => CountNumberTypeOfCake();
+    public int NumberOfEmptySlot => _settings.MaxPiecePerPlate - _cakes.Count;
 
-	public Action DestroyAction;
+    public Action DestroyAction;
 
-	private void Awake()
-	{
-		_settings = GameManager.Instance.GameSettings.PlateSettings;
-	}
+    private void Awake()
+    {
+        _settings = GameManager.Instance.GameSettings.PlateSettings;
+    }
 
-	private void OnEnable()
-	{
-		Reset();
-	}
+    private void OnEnable()
+    {
+        Reset();
+    }
 
-	private void Reset()
-	{
-		transform.localScale = Vector3.one;
-		var color = _spriteRenderer.color;
-		color.a = 1;
-		_spriteRenderer.color = color;
-	}
+    private void Reset()
+    {
+        transform.localScale = Vector3.one;
+        var color = _spriteRenderer.color;
+        color.a = 1;
+        _spriteRenderer.color = color;
+    }
 
-	public void SetOrderInLayer(int order)
-	{
-		_spriteRenderer.sortingOrder = order;
-		foreach (var c in _cakes)
-		{
-			c.SetOrderInLayer(order + 1);
-		}
-	}
+    private int CountNumberTypeOfCake()
+    {
+        var id = _cakes[0].ID;
+        var count = 1;
+        foreach (var t in _cakes)
+        {
+            if (t.ID == id) continue;
+            id = t.ID;
+            count++;
+        }
 
-	public void ResetOrderInLayer()
-	{
-		_spriteRenderer.sortingOrder = _rootSortingOrder;
-		foreach (var c in _cakes)
-		{
-			c.ResetOrderInLayer();
-		}
-	}
+        return count;
+    }
 
-	[Button(ButtonSizes.Gigantic)]
-	public void RandomCake()
-	{
-		DestroyAllCakes();
-		var numberOfCake = Random.Range(_settings.MinPiecePerPlate, _settings.MaxPiecePerPlate);
-		_cakes = GameManager.Instance.ObjectPooler.InstantiateRandomCakes(
-			GameManager.Instance.PlayerData.MinCakeLevel,
-			GameManager.Instance.PlayerData.MaxCakeLevel,
-			transform,
-			numberOfCake
-		);
-		ArrangeCake();
-		spawnPosition = transform.position;
-	}
+    private int CountNumberOfCakeWithType(int id)
+    {
+        return _cakes.Count(cake => cake.ID == id);
+    }
 
-	private void ArrangeCake()
-	{
-		_cakes = _cakes.OrderBy(c => c.ID).ToList();
-		var randomStartIndex = Random.Range(0, _settings.PiecePerPlate);
-		var angle = _settings.Angles[randomStartIndex];
-		for (var i = 0; i < _cakes.Count; i++)
-		{
-			angle.z = -i * 60;
-			_cakes[i].IndexInPlate = randomStartIndex + i;
-			_cakes[i].DoRotate(_settings.Angles[randomStartIndex], angle);
-		}
-	}
+    public void SetOrderInLayer(int order)
+    {
+        _spriteRenderer.sortingOrder = order;
+        foreach (var c in _cakes)
+        {
+            c.SetOrderInLayer(order + 1);
+        }
+    }
 
-	[Button(ButtonStyle.FoldoutButton)]
-	private void DestroyAllCakes()
-	{
-		foreach (var cake in _cakes)
-		{
-			cake?.Destroy();
-		}
+    public void ResetOrderInLayer()
+    {
+        _spriteRenderer.sortingOrder = _rootSortingOrder;
+        foreach (var c in _cakes)
+        {
+            c.ResetOrderInLayer();
+        }
+    }
 
-		_cakes.Clear();
-	}
+    [Button(ButtonSizes.Gigantic)]
+    public void RandomCake()
+    {
+        DestroyAllCakes();
+        var numberOfCake = Random.Range(_settings.MinPiecePerPlate, _settings.MaxPiecePerPlate);
+        _cakes = GameManager.Instance.ObjectPooler.InstantiateRandomCakes(
+            GameManager.Instance.PlayerData.MinCakeLevel,
+            GameManager.Instance.PlayerData.MaxCakeLevel,
+            transform,
+            numberOfCake
+        );
+        ArrangeCake();
+        spawnPosition = transform.position;
+    }
 
-	public void Destroy(bool hasAnim = true)
-	{
-		DestroyAllCakes();
-		if (hasAnim)
-		{
-			transform.DOScale(Vector3.zero, 0.3f).SetEase(Ease.InBack).OnComplete(ReturnPool);
-			var targetColor = _spriteRenderer.color;
-			targetColor.a = 0;
-			DOVirtual.Color(_spriteRenderer.color,
-					targetColor,
-					0.3f,
-					value => { _spriteRenderer.color = value; })
-				.SetEase(Ease.InBack);
-			return;
-		}
+    private void ArrangeCake()
+    {
+        _cakes = _cakes.OrderBy(c => c.ID).ToList();
+        var randomStartIndex = Random.Range(0, _settings.PiecePerPlate);
+        var angle = _settings.Angles[randomStartIndex];
+        for (var i = 0; i < _cakes.Count; i++)
+        {
+            angle.z = -i * 60;
+            _cakes[i].IndexInPlate = randomStartIndex + i;
+            _cakes[i].DoRotate(_settings.Angles[randomStartIndex], angle);
+        }
+    }
 
-		ReturnPool();
-	}
+    [Button(ButtonStyle.FoldoutButton)]
+    private void DestroyAllCakes()
+    {
+        foreach (var cake in _cakes)
+        {
+            cake?.Destroy();
+        }
 
-	private void ReturnPool()
-	{
-		DestroyAction.Invoke();
-		GameManager.Instance.ObjectPooler.DestroyPlate(gameObject);
-	}
+        _cakes.Clear();
+    }
 
-	public bool IsTouchOnPlate(Vector2 position)
-	{
-		return Mathf.Abs(position.x - transform.position.x) <=
-		       GameManager.Instance.GameSettings.BoardSettings.CellSize / 2f &&
-		       Mathf.Abs(position.y - transform.position.y) <=
-		       GameManager.Instance.GameSettings.BoardSettings.CellSize / 2f;
-	}
+    public void Destroy(bool hasAnim = true)
+    {
+        DestroyAllCakes();
+        if (hasAnim)
+        {
+            transform.DOScale(Vector3.zero, 0.3f).SetEase(Ease.InBack).OnComplete(ReturnPool);
+            var targetColor = _spriteRenderer.color;
+            targetColor.a = 0;
+            DOVirtual.Color(_spriteRenderer.color,
+                    targetColor,
+                    0.3f,
+                    value => { _spriteRenderer.color = value; })
+                .SetEase(Ease.InBack);
+            return;
+        }
 
-	public void UpdatePosition(Vector2 worldPosition)
-	{
-		transform.position = worldPosition;
-	}
+        ReturnPool();
+    }
 
-	public void ResetToSpawnPosition()
-	{
-		transform.DOKill();
-		var duration = Vector2.Distance(transform.position, spawnPosition) / 20f;
-		transform.DOMove(spawnPosition, duration).OnComplete(ResetOrderInLayer);
-	}
+    private void ReturnPool()
+    {
+        DestroyAction.Invoke();
+        GameManager.Instance.ObjectPooler.DestroyPlate(gameObject);
+    }
 
-	public void MoveToPositionOnBoard(Vector2 position)
-	{
-		transform.DOKill();
-		var duration = Vector2.Distance(transform.position, spawnPosition) / 50f;
-		transform.DOMove(position, duration).OnComplete(OnPlacedToBoard);
-	}
+    public bool IsTouchOnPlate(Vector2 position)
+    {
+        return Mathf.Abs(position.x - transform.position.x) <=
+               GameManager.Instance.GameSettings.BoardSettings.CellSize / 2f &&
+               Mathf.Abs(position.y - transform.position.y) <=
+               GameManager.Instance.GameSettings.BoardSettings.CellSize / 2f;
+    }
 
-	private void OnPlacedToBoard()
-	{
-		FindCakeNearBy();
-	}
+    public void UpdatePosition(Vector2 worldPosition)
+    {
+        transform.position = worldPosition;
+    }
 
-	private void FindCakeNearBy()
-	{
-		var id = -1;
-		foreach (var cake in _cakes.Where(cake => cake.ID != id))
-		{
-			id = cake.ID;
-			GameController.Instance.BoardController.FindNeighbourHasCakeId(this, id);
-		}
-	}
+    public void ResetToSpawnPosition()
+    {
+        transform.DOKill();
+        var duration = Vector2.Distance(transform.position, spawnPosition) / 20f;
+        transform.DOMove(spawnPosition, duration).OnComplete(ResetOrderInLayer);
+    }
 
-	public void AddCake(Cake cake)
-	{
-		cake.transform.SetParent(transform);
-		cake.MoveToPlate(this);
-		cake.IndexInPlate = LastIndex;
-		_cakes.Add(cake);
-		StartCoroutine(IECheckComplete());
-	}
+    public void MoveToPositionOnBoard(Vector2 position)
+    {
+        transform.DOKill();
+        var duration = Vector2.Distance(transform.position, spawnPosition) / 50f;
+        transform.DOMove(position, duration).OnComplete(OnPlacedToBoard);
+    }
 
-	private IEnumerator IECheckComplete()
-	{
-		yield return new WaitForSeconds(0.5f);
-		if (!IsCompleted) yield break;
-		Destroy();
-	}
+    private void OnPlacedToBoard()
+    {
+        FindCakeNearBy();
+    }
 
-	public bool HasCake(int id)
-	{
-		return _cakes.Any(t => t.ID == id);
-	}
+    private void FindCakeNearBy()
+    {
+        var id = -1;
+        foreach (var cake in _cakes)
+        {
+            if (id == cake.ID) continue;
+            id = cake.ID;
+            GameController.Instance.BoardController.FindNeighbourHasCakeId(this, id);
+        }
+    }
 
-	private bool CheckComplete()
-	{
-		var id = _cakes[0].ID;
-		var count = 0;
-		for (var i = 0; i < _cakes.Count; i++)
-		{
-			if (_cakes[i].ID != id)
-				return false;
-			count++;
-		}
+    public void AddCake(Cake cake)
+    {
+        cake.transform.SetParent(transform);
+        cake.MoveToPlate(this);
+        cake.IndexInPlate = LastIndex;
+        _cakes.Add(cake);
+        if (!IsFullOfCake)
+            return;
+        StartCoroutine(IECheckComplete());
+    }
 
-		return count == _settings.MaxPiecePerPlate;
-	}
+    private IEnumerator IECheckComplete()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (!IsCompleted) yield break;
+        Destroy();
+    }
 
-	private bool IsEmpty()
-	{
-		return _cakes.Count == 0;
-	}
+    public bool HasCake(int id)
+    {
+        return _cakes.Any(t => t.ID == id);
+    }
 
-	public void MoveCakeToTarget(Plate plate, int id)
-	{
-		var removedCakes = new List<Cake>();
-		foreach (var t in _cakes.Where(t => t.ID == id))
-		{
-			if (plate.IsFullOfCake)
-				return;
-			plate.AddCake(t);
-			removedCakes.Add(t);
-		}
+    private bool CheckComplete()
+    {
+        var id = _cakes[0].ID;
+        var count = 0;
+        for (var i = 0; i < _cakes.Count; i++)
+        {
+            if (_cakes[i].ID != id)
+                return false;
+            count++;
+        }
 
-		foreach (var t in removedCakes)
-		{
-			_cakes.Remove(t);
-		}
+        return count == _settings.MaxPiecePerPlate;
+    }
 
-		if (IsEmpty())
-		{
-			Destroy();
-			return;
-		}
+    private bool IsEmpty()
+    {
+        return _cakes.Count == 0;
+    }
 
-		FindCakeNearBy();
-	}
+    public void MoveCakeToTarget(Plate plate, int id)
+    {
+        var removedCakes = new List<Cake>();
+        foreach (var t in _cakes.Where(t => t.ID == id))
+        {
+            if (plate.IsFullOfCake)
+                continue;
+            plate.AddCake(t);
+            removedCakes.Add(t);
+        }
+
+        foreach (var t in removedCakes)
+        {
+            _cakes.Remove(t);
+        }
+
+        if (IsEmpty())
+        {
+            Destroy();
+            return;
+        }
+
+        ArrangeCake();
+        // FindCakeNearBy();
+    }
 }
